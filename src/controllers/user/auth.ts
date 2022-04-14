fe69858229099adcb0cdabbe09942ed3a15e2806
hokim <rlaghekd777@naver.com>
2022-04-14 21:16:41 +0900

    Add. signup, login구현(서비스코드 분리 필요)

--- src/controllers/user/auth.ts
+++ src/controllers/user/auth.ts
@@ -2,8 +2,44 @@ import express, { NextFunction, Response, Request } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from '../../models/user';
+import passport from 'passport';
 
 export const router = express.Router();
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    passport.authenticate('local', (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).send('error');
+      }
+      req.login(user, { session: false }, (loginError) => {
+        if (loginError) {
+          return res.status(400).send(loginError);
+        }
+        const token = jwt.sign(
+          {
+            username: user.username,
+            password: user.password,
+          },
+          process.env.jwtSecret,
+        );
+        return res.status(200).json({
+          token,
+          message: 'success',
+        });
+      });
+    })(req, res);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+export const tokenTest = async (req: Request, res: Response, next: NextFunction) => {
+  return res.json({
+    message: 'success',
+  });
+};
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
@@ -33,76 +69,3 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
     return next(error);
   }
 };
-
-export const tokenTest = async (req: Request, res: Response, next: NextFunction) => {
-  console.log('tokenTest');
-  try {
-    res.json({
-      result: true,
-    });
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-};
-
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-  const { username, password } = req.body;
-  try {
-    const exUser = await User.findOne({ where: { username } });
-    if (exUser) {
-      const result = await bcrypt.compare(password, exUser.password);
-      if (result) {
-        const token = jwt.sign(
-          {
-            username: exUser.username,
-            password: exUser.password,
-          },
-          process.env.jwtSecret,
-          {
-            expiresIn: '20m',
-            issuer: 'kimho',
-          },
-        );
-        return res.json({
-          code: 200,
-          token,
-        });
-      }
-    }
-    return res.status(401).json({
-      code: 401,
-      message: '아이디 또는 비밀번호가 잘못되었습니다',
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: '다시 입력해주세요',
-    });
-  }
-};
-
-// passport-local
-// export const login = (req: Request, res: Response, next: NextFunction) => {
-//   passport.authenticate('local', (authError, user, info) => {
-//     if (authError) {
-//       console.error(authError);
-//       return next(authError);
-//     }
-//     if (!user) {
-//       return res
-//         .json({
-//           message: info.message,
-//         })
-//         .redirect('/');
-//     }
-//     return req.login(user, (loginError) => {
-//       if (loginError) {
-//         console.error(loginError);
-//         return next(loginError);
-//       }
-//       return res.redirect('/');
-//     });
-//   })(req, res, next);
-// };
