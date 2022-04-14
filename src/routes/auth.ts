8605e3e9a3ccf5fe43ae6aef7763c4d05a7bbf88
hokim <rlaghekd777@naver.com>
2022-04-14 21:17:00 +0900

    Add. sighup, login routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
 import * as authRouter from '../controllers/user/auth';
 import passport from 'passport';
 
@@ -6,5 +6,4 @@ export const router = express.Router();
 
 router.post('/signup', authRouter.signup);
 router.post('/login', authRouter.login);
-router.get('/test', passport.authenticate('jwt', { session: false }), authRouter.tokenTest);
-//router.use('/auth', authRouter);
+router.post('/test', passport.authenticate('jwt', { session: false }), authRouter.tokenTest);
